2770a415872f0bfd1f752ccaa70f1dd08ab92a7d
LeeMeiLing <leem0060@outlook.com>
2023-04-19 10:45:37 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Friend } from './models';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'day33';
+
+  selectedFriend!:Friend
+
+  friends:Friend[] = [
+    {name:'barney', email:'barney@gmail.com',phone:'23456'},
+    {name:'betty', email:'betty@gmail.com',phone:'13456'}
+  ]
+
+  process(friend:Friend){
+    
+    let idx = this.friends.findIndex(f => f.name == friend.name)
+
+    if(idx==-1)
+      this.friends.push(friend)
+    else
+      this.friends[idx] = friend
+
+  }
+
+  selection(name:string){
+    console.info('>>> fr selected: ',name)
+    const fr = this.friends.find(f => f.name == name)
+    //@ts-ignore
+    this.selectedFriend = fr
+  }
 }
---
+++ src/app/components/friends.component.ts
@@ -0,0 +1,60 @@
+import { Component, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Friend } from '../models';
+import { Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-friends',
+  templateUrl: './friends.component.html',
+  styleUrls: ['./friends.component.css']
+})
+export class FriendsComponent implements OnInit, OnChanges{
+
+  form!:FormGroup
+
+  @Input()
+  friend!:Friend
+
+  @Output()
+  onFriend = new Subject<Friend>
+
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(): void {
+    this.form = this.createForm(this.friend) // can pass in undefined argument ??
+    console.info('friend: ',this.friend)
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // console.log('>>> changes:', changes)
+    const fr = changes['friend'].currentValue as Friend
+    this.form = this.createForm(fr)
+  }
+
+  // private createForm():FormGroup{
+  //   return this.fb.group({
+  //     name:this.fb.control('',[Validators.required]),
+  //     email:this.fb.control('',[Validators.required,Validators.email]),
+  //     phone:this.fb.control('',[Validators.required])
+      
+  //   })
+  // }
+
+  private createForm(friend:Friend):FormGroup{
+    return this.fb.group({
+      name:this.fb.control( friend?friend.name:'' , [Validators.required]),
+      email:this.fb.control(friend?friend.email:'' ,[Validators.required,Validators.email]),
+      phone:this.fb.control(friend?friend.phone:'' ,[Validators.required])
+      
+    })
+  }
+
+  processForm(){
+    const fr:Friend = this.form.value
+    console.info('>>> fr:',fr)
+    this.onFriend.next(fr)
+    this.form.reset //  this will not reset the form-array, better recreate the form if it is complex form
+  }
+
+  
+}
